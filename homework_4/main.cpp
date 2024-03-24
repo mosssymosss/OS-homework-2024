de4f74bf47b7ee052cf852309233ae2ad8d1fc83
Anahit <anah.arakelyan@gmail.com>
2024-03-25 01:44:34 +0400

    added ftruncate, added libraries

--- homework_4/main.cpp
+++ homework_4/main.cpp
@@ -13,16 +13,27 @@ void philosopher(int id, sem_t* sems)
 {
 	while (true) 
 	{
+        int priorFork = id == 4 ? 0 : id;
+        int nonPriorFork = id == 4 ? 4 : id + 1;
+
 		std::cout << id << ": is thinking" << std::endl;
 		sleep(rand() % 3);
 
-		sem_wait(&sems[id]);
+		if(sem_wait(&sems[priorFork]) < 0)
+        {
+            std::cerr << "sem_wait" << std::endl;
+            exit(EXIT_FAILURE);
+        }
 		std::cout << id << ": picked up left chopstick" << std::endl;
 
-		if(sem_trywait(&sems[(id + 1) % SIZE]) == -1)
+		if(sem_trywait(&sems[nonPriorFork]) == -1)
 		{
-			sem_post(&sems[id]);
-			std::cout << id << ": released chopstick" << std::endl;
+			if(sem_post(&sems[priorFork]) < 0)
+            {
+                std::cerr << "sem_post" << std::endl;
+                exit(EXIT_FAILURE);
+            }
+			std::cout << id << ": released one chopstick" << std::endl;
 		}
 		else
 		{
@@ -30,8 +41,16 @@ void philosopher(int id, sem_t* sems)
 			std::cout << id << ": is eating" << std::endl;
 			sleep(rand() % 3);
 
-			sem_post(&sems[id]);
-			sem_post(&sems[(id + 1) % SIZE]);
+			if(sem_post(&sems[priorFork]) < 0)
+            {
+                std::cerr << "sem_post" << std::endl;
+                exit(EXIT_FAILURE);
+            }
+			if(sem_post(&sems[nonPriorFork]) < 0)
+            {
+                std::cerr << "sem_post" << std::endl;
+                exit(EXIT_FAILURE);
+            }
 			std::cout << id << ": released chopsticks" << std::endl;
 		}	
 
@@ -41,17 +60,26 @@ void philosopher(int id, sem_t* sems)
 
 int main() 
 {
-	int shm_fd = shm_open("/shared_mem", O_CREAT | O_RDWR, 0666);
+	int shm_fd = shm_open("/shared_mem", O_CREAT | O_RDWR | O_TRUNC, 0666);
 	if (shm_fd == -1) 
 	{
 		std::cerr << "shm_open failed" << std::endl;
 		exit(EXIT_FAILURE);
 	}
-
+    int trunc = ftruncate(shm_fd, SIZE * sizeof(sem_t));
+    if(trunc == -1)
+    {
+        std::cerr << "ftruncate" << std::endl;
+        exit(EXIT_FAILURE);
+    }
 	sem_t* sems = (sem_t*)mmap(NULL, SIZE * sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, shm_fd, 0);
 	for (int i = 0; i < SIZE; ++i) 
 	{
-		sem_init(&sems[i], 1, 1);
+        if(sem_init(&sems[i], 1, 1) < 0)
+        {
+            std::cerr << "sem_destroy" << std::endl;
+            exit(EXIT_FAILURE);
+        }
 	}
 
 	std::vector<int> pids(5);
@@ -75,10 +103,19 @@ int main()
 					
 	for(int i = 0; i < SIZE; ++i) 
 	{
-		sem_destroy(&sems[i]);
+		if(sem_destroy(&sems[i]) < 0)
+        {
+            std::cerr << "sem_destroy" << std::endl;
+            exit(EXIT_FAILURE);
+        }
 	}
 						
 	munmap(sems, SIZE * sizeof(sem_t));
 	shm_unlink("/shared_mem");
-	return 0;
+    if(close(shm_fd) < 0)
+    {
+        std::perror("close");
+        exit(errno);
+    }
+    return 0;
 }
