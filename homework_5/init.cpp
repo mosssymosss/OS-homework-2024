1620722b0bb1f46480a21d835f26536d94507329
Anahit <anah.arakelyan@gmail.com>
2024-04-08 15:58:23 +0400

    added init file

---
+++ homework_5/init.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cstdlib>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <cstring>
+#include <cerrno>
+
+#include "bank.h"
+
+int main()
+{
+    const char* shm_name = "/bank_shared_mem";
+    const int n = 10;
+    int shm_fd = shm_open(shm_name, O_CREAT | O_RDWR, 066);
+
+    std::size_t size = sizeof(Bank) + n * sizeof(BankCell);
+
+    if(ftruncate(shm_fd, size) == -1)
+    {
+        std::cerr << "init ftruncate"<<std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    void* ptr = mmap(nullptr, size, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    if(ptr == MAP_FAILED)
+    {
+        std::cerr << "init mmap" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    Bank* bnk = new (ptr) Bank(n);
+
+    if(munmap(ptr, size) == -1)
+    {
+        std::cerr << "init munmap" <<std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    if(close(shm_fd) == -1)
+    {
+        std::cerr << "init close" <<std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    return 0;
+}
